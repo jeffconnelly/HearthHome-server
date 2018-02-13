22a9a5fd7492aa88fa677addf8d67a6ecaec696d
Jeff Connelly <jeff.connelly6@gmail.com>
2018-02-13 12:07:05 -0700

    cors works for netlify, added travis

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const morgan = require('morgan');
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const cors = require('cors');
 
 const { router: deckRouter } = require('./deckrouter');
 const { router: usersRouter } = require('./users');
@@ -23,11 +22,15 @@ app.use(express.static('public'));
 app.use(morgan('common'));
 
 //CORS function
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
-);
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 
 // Passport import
 passport.use(localStrategy);
